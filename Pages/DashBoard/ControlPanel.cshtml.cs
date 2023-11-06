43cd66c310bd7eae31bb3d84c3549163dfa98eb8
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2023-11-06 12:38:15 +0100

    Prøver at lave dashboard

---
+++ Pages/DashBoard/ControlPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigMammaPizzaGroup.Pages.DashBoard
+{
+    public class ControlPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
