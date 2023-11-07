9b528d5000ec4f8fab3aefada9a188815288b581
Marco Dybig <mad006@edu.zealand.dk>
2023-11-07 21:13:31 +0100

    pris sort

--- Pages/PizzaMenu/Index.cshtml.cs
+++ Pages/PizzaMenu/Index.cshtml.cs
@@ -16,6 +16,7 @@ namespace BigMammaPizzaGroup.Pages.Menukort
             _repo = repo;
         }
         //property til viewet
+        public static int Sort { get; set; }
         public int NytPizzaNummer { get; set; }
         Items item { get; set; }
         public Items Tilføjet { get; set; }
@@ -26,23 +27,41 @@ namespace BigMammaPizzaGroup.Pages.Menukort
         {
             //AllItems = _repo.GetAllPizzas();
             //_repo.AddNumbers();
+            Sort = 1;
                 AllItems = _repo.GetAllItems();
         }
 
         public IActionResult OnPostPris()
         {   
-            AllItems = _repo.SortItemsPrice();
+            if (Sort == 1||Sort == 3)
+            {
+                AllItems = _repo.SortItemsPrice();
+                Sort = 2;
+            }
+            else if (Sort ==2)
+            {
+                AllItems = _repo.SortItemsPrice();
+                AllItems.Reverse();
+                Sort = 3;
+            }
+           
             return Page();
         }
         public IActionResult OnPostNummer()
         {
+            Sort = 1;
             AllItems = _repo.GetAllItems();
             return Page();
         }
         public IActionResult OnPostTilføj(int nummer)
         {
             Items item = _repo.SearchItem(nummer);
-            AllItems = _repo.GetAllItems();
+            switch (Sort)
+            {
+                case 1: AllItems = _repo.GetAllItems(); break;
+                case 2: AllItems = _repo.SortItemsPrice(); break;
+                case 3: AllItems = _repo.SortItemsPrice(); AllItems.Reverse(); break;
+            }
             Tilføjet = item;
 
             return Page();
